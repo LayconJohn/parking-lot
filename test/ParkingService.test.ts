c1bcdc66ffd7edae968ca8a06df7908f12984309
Laycon Gomes <laycon.gomes@tld.com.br>
2025-01-14 14:11:04 -0300

    test(ParkingService): update ParkingService

--- test/ParkingService.test.ts
+++ test/ParkingService.test.ts
@@ -1,8 +1,15 @@
+import ParkedCarDAOMemory from "../src/ParkedCarDAOMemory";
 import ParkingService from "../src/ParkingService";
+
+let parkingService: ParkingService;
+
+beforeAll(() => {
+    const parkedCarDAO = new ParkedCarDAOMemory();
+    parkingService = new ParkingService(parkedCarDAO);
+});
 
 test("Deve entrar e sair um carro do estacionamento, calculando um valor da tarifa, 10 reais por hora de permanencia", async () => {
     //Given
-    const parkingService = new ParkingService();
     const plate = "AAA1111";
 
     //When
@@ -17,12 +24,10 @@ test("Deve entrar e sair um carro do estacionamento, calculando um valor da tari
 });
 
 test("Deve lançar um erro quando o carro não for encontrado tentar sair", async () => {
-    const parkingService = new ParkingService();
     await expect(() => parkingService.checkout("ABA1212", new Date("2025-01-14T14:00:00"))).rejects.toThrow(new Error(`ABA1212 not parked`))
 });
 
 test("Não deve entrar carro com placa inválida", async () => {
-    const parkingService = new ParkingService();
     const plate = "AA11";
     const checkinDate = new Date("2025-01-13T10:00:00");
     await expect(() => parkingService.checkin(plate, checkinDate)).rejects.toThrow(new Error("Invalid plate"))
